9ca6a9889c8857fb7f8829368185c69c8c332fd6
abraza123 <53875752+abraza123@users.noreply.github.com>
2019-08-27 11:28:32 -0400

    Add files via upload

---
+++ Design Pattern 2/Living.java
@@ -0,0 +1,24 @@
+public interface Living{
+    public void setName(String newName); 
+    public String getName();
+
+    public void setHeight(double newHeight);
+    public double getHeight();
+
+    public void setWeight(double newWeight);
+    public double getWeight();
+
+    public void setFavFood(String newFood);
+    public String getFaveFood();
+
+    public void setSpeed(double newSpeed);
+    public double getSpeed();
+
+    private void setSound(String newSound);
+    public String getSound();
+
+
+
+        
+    
+}
---
+++ Design Pattern 2/Monkey.java
@@ -0,0 +1,25 @@
+public class Monkey implements Living{
+
+    @Override 
+    public void setName(String newName){
+
+
+    }
+
+    @Override
+    public String getName(){
+        return null;
+
+    } 
+
+    public void setHeight(double newHeight) {
+        
+    }
+
+    public double getHeight(){
+        return null;
+
+    }
+    
+}
+    
